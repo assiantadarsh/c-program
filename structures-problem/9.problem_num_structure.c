4b3f81287a9b27f3831bd63ab234925db31b3b71
Adarsh shukla <adarshbhai917963@gmail.com>
2026-03-19 00:52:28 +0530

    Implement date comparison in structure example

--- structures-problem/9.problem_num_structure.c
+++ structures-problem/9.problem_num_structure.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
-// Create a structure 'date' that contains three members namely date , month and year.Create 2 structure variables with different dates and now compare the two . If the dates are equal then display message as "equal" otherwise "Unequal".
+#include<stdbool.h>
 
+int main(){
+// Create a structure 'date' that contains three members namely date , month and year.Create 2 structure variables
+// with different dates and now compare the two . If the dates are equal then display message as "equal" otherwise "Unequal".
+bool flag = true;
 typedef struct date{
     int day;
     int month;
@@ -10,15 +13,26 @@ typedef struct date{
     
 }date;
 
-date a , b;
-a.day = 12;
-a.month = 4;
-a.year = 2006;
+date a,b;
+a.day = 23;
+a.month = 2;
+a.year = 2023;
 
 b.day = 23;
-b.month = 6;
-b.year = 2007;
+b.month = 12;
+b.year = 2023;
 
+if(a.day != b.day) flag= false;
+if(a.month != b.month) flag= false;
+if(a.year != b.year) flag= false;
 
-
+if(flag == true){
+    printf("Equal");}
+else{
+    printf("Unequal");
 }
+return 0;
+}
+
+
+
